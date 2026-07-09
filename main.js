7d12cf8aad3d9ed5987b219a55dcf38a07ed7c4d
TheGreatOne57 <83248294+TheGreatOne57@users.noreply.github.com>
2026-07-09 06:47:35 -0400

    Update main.js

--- main.js
+++ main.js
@@ -37,3 +37,22 @@ const observer = new IntersectionObserver((entries) => {
 memories.forEach(memory => {
     observer.observe(memory);
 });
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:0.2
+});
+
+document
+.querySelectorAll(".hidden")
+.forEach(el=>observer.observe(el));
