aa77249223214a0823b5dc3073d358e3685df95e
TheGreatOne57 <83248294+TheGreatOne57@users.noreply.github.com>
2026-07-09 08:11:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -19,24 +19,6 @@ openButton.addEventListener("click", () => {
     letter.classList.remove("hidden");
 
 });
-
-const memories = document.querySelectorAll(".memory");
-
-const observer = new IntersectionObserver((entries) => {
-
-    entries.forEach(entry => {
-
-        if (entry.isIntersecting) {
-            entry.target.classList.add("show");
-        }
-
-    });
-
-});
-
-memories.forEach(memory => {
-    observer.observe(memory);
-});
 const observer = new IntersectionObserver((entries)=>{
 
     entries.forEach(entry=>{
